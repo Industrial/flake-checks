60435910f9314bf4a4e523f1bbac7a6de69f0ba1
Tom Wieland <tom.wieland@gmail.com>
2024-08-06 18:57:05 +0000

    fix(lib,docs): provide options and run only hooks configured to run, update documentation

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # flake-checks
 
-A reusable Nix flake for adding tests (`nix-unit`) to a flake in a standardized way.
+A reusable Nix flake for adding checks to a flake in a standardized way. It
+defined one pre-commit check that I use in all my flakes.
 
 ## Usage
 
 ```nix
 {
-  description = "A reusable Nix flake for adding tests (nix-unit) to a flake in a standardized way.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     for-all-systems.url = "github:Industrial/for-all-systems";
@@ -21,6 +21,22 @@ A reusable Nix flake for adding tests (`nix-unit`) to a flake in a standardized 
       system,
       pkgs,
     }:
-      flake-checks.checks {inherit inputs system pkgs;});
+      flake-checks.checks {inherit inputs system;});
   };
 }
+```
+
+## Options
+
+| Name                 | Default | Description                              |
+| -------------------- | ------- | ---------------------------------------- |
+| run-nix-hooks        | true    | Wether to run the nix hooks.             |
+| run-bash-hooks       | true    | Wether to run the bash hooks.            |
+| run-markdown-hooks   | true    | Wether to run the markdown hooks.        |
+| run-yaml-hooks       | true    | Wether to run the yaml hooks.            |
+| run-toml-hooks       | true    | Wether to run the toml hooks.            |
+| run-json-hooks       | true    | Wether to run the json hooks.            |
+| run-git-hooks        | true    | Wether to run the git hooks.             |
+| run-typescript-hooks | true    | Wether to run the typescript hooks.      |
+| run-generic-hooks    | true    | Wether to run the generic hooks.         |
+| run-unit-tests       | true    | Wether to run the unit tests (nix-unit). |
--- lib/checks.nix
+++ lib/checks.nix
@@ -1,47 +1,87 @@
 {
   inputs,
   system,
-  ...
-}: {
-  pre-commit-check = inputs.git-hooks.lib.${system}.run {
-    src = ./..;
-    hooks = {
-      # Nix
+  run-nix-hooks ? true,
+  run-bash-hooks ? true,
+  run-markdown-hooks ? true,
+  run-yaml-hooks ? true,
+  run-toml-hooks ? true,
+  run-json-hooks ? true,
+  run-git-hooks ? true,
+  run-typescript-hooks ? true,
+  run-generic-hooks ? true,
+  run-unit-tests ? true,
+}: let
+  nix-hooks =
+    if run-nix-hooks
+    then {
       alejandra.enable = true;
       deadnix.enable = true;
       flake-checker.enable = true;
+    }
+    else {};
 
-      # Bash
+  bash-hooks =
+    if run-bash-hooks
+    then {
       shellcheck.enable = true;
       beautysh.enable = true;
+    }
+    else {};
 
-      # Markdown
+  markdown-hooks =
+    if run-markdown-hooks
+    then {
       markdownlint.enable = true;
+    }
+    else {};
 
-      # YAML
+  yaml-hooks =
+    if run-yaml-hooks
+    then {
       check-yaml.enable = true;
       yamllint.enable = true;
       # yamlfmt.enable = true;
+    }
+    else {};
 
-      # TOML
+  toml-hooks =
+    if run-toml-hooks
+    then {
       check-toml.enable = true;
       taplo.enable = true;
+    }
+    else {};
 
-      # JSON
+  json-hooks =
+    if run-json-hooks
+    then {
       check-json.enable = true;
       pretty-format-json.enable = true;
+    }
+    else {};
 
-      # Git
+  git-hooks =
+    if run-git-hooks
+    then {
       check-merge-conflicts.enable = true;
       commitizen = {
         enable = true;
         stages = ["commit-msg"];
       };
+    }
+    else {};
 
-      # TypeScript / JavaScript
+  typescript-hooks =
+    if run-typescript-hooks
+    then {
       eslint.enable = true;
+    }
+    else {};
 
-      # Generic
+  generic-hooks =
+    if run-generic-hooks
+    then {
       check-added-large-files.enable = true;
       check-case-conflicts.enable = true;
       check-executables-have-shebangs.enable = true;
@@ -53,14 +93,34 @@
       fix-byte-order-marker.enable = true;
       forbid-new-submodules.enable = true;
       trim-trailing-whitespace.enable = true;
+    }
+    else {};
 
-      unit-tests = {
-        enable = true;
-        name = "Unit tests";
-        entry = "nix run nixpkgs#nix-unit -- --flake .#tests";
-        pass_filenames = false;
-        stages = ["pre-push"];
-      };
-    };
+  unit-tests =
+    if run-unit-tests
+    then {
+      enable = true;
+      name = "Unit tests";
+      entry = "nix run nixpkgs#nix-unit -- --flake .#tests";
+      pass_filenames = false;
+      stages = ["pre-push"];
+    }
+    else {};
+
+  all-hooks =
+    nix-hooks
+    // bash-hooks
+    // markdown-hooks
+    // yaml-hooks
+    // toml-hooks
+    // json-hooks
+    // git-hooks
+    // typescript-hooks
+    // generic-hooks
+    // unit-tests;
+in {
+  pre-commit-check = inputs.git-hooks.lib.${system}.run {
+    src = ./..;
+    hooks = all-hooks;
   };
 }
